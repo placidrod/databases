bfc736536f234f67f6ef04efd1fb68a6be1b0381
Placid Rodrigues <placidrod@gmail.com>
2017-06-13 12:30:17 -0700

    Create schema

--- server/schema.sql
+++ server/schema.sql
@@ -2,14 +2,35 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+DROP TABLE IF EXISTS `messages`;
+
+CREATE TABLE `messages` (
+  `objectId` INTEGER NOT NULL AUTO_INCREMENT,
+  `text` VARCHAR(256) NULL DEFAULT NULL,
+  `user` INTEGER NULL DEFAULT NULL,
+  `room` INTEGER NULL DEFAULT NULL,
+  `createdAt` DATETIME NULL DEFAULT NULL,
+  `updatedAt` DATETIME NULL DEFAULT NULL,
+  PRIMARY KEY (`objectId`)
 );
 
 /* Create other tables and define schemas for them here! */
 
+DROP TABLE IF EXISTS `users`;
 
+CREATE TABLE `users` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(60) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
 
+DROP TABLE IF EXISTS `rooms`;
+
+CREATE TABLE `rooms` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `name` INTEGER NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
